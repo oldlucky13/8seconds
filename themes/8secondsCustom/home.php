26ba680885c11f6d930a37c6bc046c0fc0d10e7c
Brian Friel <briandfriel@gmail.com>
2017-02-21 09:34:23 -0800

    add DS_Store to gitignore

--- themes/8secondsCustom/home.php
+++ themes/8secondsCustom/home.php
@@ -19,23 +19,33 @@ get_header();
       <span class="middle"></span>
       <span class="bottom"></span>
     </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          hi
+        </div>
+        <div class="col-md-6">
+          kids
+        </div>
+      </div>
+    </div>
     <div class="menuOverlay" id="overlayMenu">
       <nav class="overlay-menu">
         <ul id="nav-menu">
-          
+
         </ul>
       </nav>
     </div>
   </div>
-  
-  <div id="ageGate" class="modal">
+
+  <!-- <div id="ageGate" class="modal">
       <img src="/wp-content/uploads/2017/01/Jseeds-ageGate_mini.png">
       <h1>Are you over 21 years old?</h1>
       <div class="button-wrapper">
           <button id="over21" class="JSbtn">Yes</button>
           <button id="under21" class="JSbtn">No</button>
       </div>
-  </div>
+  </div> -->
 </div> <!-- #main-content -->
 
 
