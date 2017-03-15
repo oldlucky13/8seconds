1e07d8665194e30376aab5f9930fa37df95f72b3
Brian Friel <briandfriel@gmail.com>
2017-03-15 09:50:55 -0700

    wip: begin add text

--- themes/8secondsCustom/home.php
+++ themes/8secondsCustom/home.php
@@ -24,7 +24,7 @@ get_header();
         <div id="one" class="show-off row main-page-slide">
           <div class="no-mobile-col col-md-6 main-page-txt">
             <h1 style="text-align: center">one</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             <div class="icon-section">
               <p class="icon-section-line"></p>
             </div>
